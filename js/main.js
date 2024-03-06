2b1f015151d06a8eb8d536cb044459d0ad5cc353
Alexandr-77 <62809713+Alexandr-77@users.noreply.github.com>
2024-03-07 00:07:47 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// 
+// ================================================== 
+let burger = document.querySelector('.burger');
+let nav = document.querySelector('.header__block-nav');
+let liHeader = document.querySelectorAll('.header__menu-item');
+
+burger.addEventListener('click', rotateBurger); 
+
+for(let i = 0; i < liHeader.length; i++ ) {
+	if (window.innerWidth < 990 ) {
+		liHeader[i].addEventListener('click', rotateBurger);
+	}
+}
+
+
+function rotateBurger() { 
+  burger.classList.toggle('burger-active');
+
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
+}
+
+
+// ======== akkardion ========
+class ItcAccordion {
+	constructor(target, config) {
+	  this._el = typeof target === 'string' ? document.querySelector(target) : target;
+	  const defaultConfig = {
+		 alwaysOpen: true
+	  };
+	  this._config = Object.assign(defaultConfig, config);
+	  this.addEventListener();
+	}
+	addEventListener() {
+	  this._el.addEventListener('click', (e) => {
+		 const elHeader = e.target.closest('.accordion__header');
+		 if (!elHeader) {
+			return;
+		 }
+		 if (!this._config.alwaysOpen) {
+			const elOpenItem = this._el.querySelector('.accordion__item_show');
+			if (elOpenItem) {
+			  elOpenItem !== elHeader.parentElement ? elOpenItem.classList.toggle('accordion__item_show') : null;
+			}
+		 }
+		 elHeader.parentElement.classList.toggle('accordion__item_show');
+	  });
+	}
+ }
+
+ new ItcAccordion('#accordion-1');
+// ======== akkardion / ========
